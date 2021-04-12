da170478f7527dc27b70702410f144c71895482f
nathan hains <nathanhains14@yahoo.com>
2021-04-12 10:54:44 -0400

    commit tree e8e5e59f2c95d26c1f72d45042d431b35c9760f8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 10}
+    validates :summary, length: {maximum: 10}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    include ActiveModel::Validations
+    validates_with TitleValidator
 end
